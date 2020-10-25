52c1eb9a5461dda061b98d41be44ea1f0fa694cb
MadFoxBLR <56344115+MadFoxBLR@users.noreply.github.com>
2020-10-25 15:29:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 module.exports = function towelSort (matrix) {
-  return Array.isArray(matrix) ? matrix.map((array, i)
-    => i % 2 ? array.reverse() : array).flat() : [];
+  return Array.isArray(matrix) ? matrix.map((array, i) => i % 2 ? array.reverse() : array).flat() : [];
 }
